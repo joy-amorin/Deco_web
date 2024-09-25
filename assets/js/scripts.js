20a7585acaab4dc6b1bc16d9631bd5544ac8c7a9
joy-amorin <amorinkaren2021@gmail.com>
2024-09-25 14:31:24 -0300

    replace lightbox library  with javascript for gallery transitions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -38,3 +38,62 @@ document.addEventListener('click', function(event) {
         });
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll('.gallery-image');
+    const modal = document.getElementById('image-modal');
+    const modalImage = document.getElementById('modal-image');
+    const closeModal = document.getElementById('close-modal');
+    const overlay = document.getElementById('overlay');
+
+    let currentImageIndex = 0;
+
+    images.forEach((image, index) => {
+        image.addEventListener('click', function(e) {
+            currentImageIndex = index; // Guarda el índice de la imagen actual
+            e.preventDefault();
+            modalImage.src = this.src;
+            overlay.style.display = "block";
+            modal.style.display = "block";
+        });
+    });
+
+    closeModal.addEventListener('click', function() {
+        modal.style.display = "none";
+        overlay.style.display = "none";
+    });
+
+    // Cierra el modal al hacer clic en el overlay
+    overlay.addEventListener('click', function() {
+        modal.style.display = "none";
+        overlay.style.display = "none";
+    });
+
+    // Navegación previa
+    document.getElementById('prev-button').addEventListener('click', function() {
+        currentImageIndex = (currentImageIndex === 0) ? images.length - 1 : currentImageIndex - 1;
+        modalImage.src = images[currentImageIndex].src;
+    });
+
+    // Navegación siguiente
+    document.getElementById('next-button').addEventListener('click', function() {
+        currentImageIndex = (currentImageIndex === images.length - 1) ? 0 : currentImageIndex + 1;
+        modalImage.src = images[currentImageIndex].src;
+    });
+
+    document.addEventListener("keydown", function(event) {
+        if (modal.style.display === "block") {
+            if (event.key === "Escape") {
+                modal.style.display = "none";
+                overlay.style.display = "none";
+            } else if (event.key === "ArrowLeft") {
+                currentImageIndex = (currentImageIndex === 0) ? images.length - 1 : currentImageIndex - 1;
+                modalImage.src = images[currentImageIndex].src;
+            } else if (event.key === "ArrowRight") {
+                currentImageIndex = (currentImageIndex === images.length - 1) ? 0 : currentImageIndex + 1;
+                modalImage.src = images[currentImageIndex].src;
+            }
+        }
+    });    
+});
+
